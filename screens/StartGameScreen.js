dfe6b4e4ff9a378b00a15253ba5202085843b35d
olympia <owojcik7@gmail.com>
2019-12-08 20:00:32 -0500

    Work on Layout by adding styling to StartGameScreen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native"
+import { View, Text, StyleSheet, TextInput, Button } from "react-native"
 
 const StartGameScreen = props => {
     return (
-        <View styles={styles.screen}>
-            <Text>The Game Screen!</Text>
+        <View style={styles.screen}>
+            <Text style={styles.title}>The Game Screen!</Text>
+            <View style={styles.inputContainer}>
+                <Text>Select a Number</Text>
+                <TextInput />
+                <View styles={styles.buttonContainer}>
+                    <Button title="Reset" onPress={() => { }} />
+                    <Button title="Guess" onPress={() => { }} />
+                </View>
+            </View>
         </View>
     )
 }
@@ -14,8 +22,26 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         flex: 1,
         padding: 10,
-
-
+        width: '100%',
+        borderBottomColor: 'pink'
+    },
+    buttonContainer: {
+        flexDirection: "row",
+        //use width of parent and position buttons according to the view
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
+    },
+    title: {
+        fontSize: 20,
+        //replaces margin bottom and margin top
+        marginVertical: 10
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        //center items horizontally on cross axis
+        alignItems: 'center'
     }
 })
 
