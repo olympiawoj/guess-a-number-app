8ed99152b7641ea2e4ae92a2ab078868c9f41e37
olympia <owojcik7@gmail.com>
2019-12-10 07:53:52 -0500

    Finish the Confirmation Box extracting NumberContainer to a component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet, Button, TouchableWithoutFeedback, Keyboard, Ale
 import Card from "../components/Card"
 import Colors from "../constants/colors"
 import Input from "../components/Input"
+import NumberContainer from "../components/NumberContainer"
 
 const StartGameScreen = props => {
 
@@ -29,11 +30,19 @@ const StartGameScreen = props => {
         setConfirmed(true)
         setSelectedNumber(chosenNumber)
         setEnteredValue('')
+        Keyboard.dismiss()
     }
 
     let confirmedOutput;
     if (confirmed) {
-        confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>
+        confirmedOutput = (
+            <Card styles={styles.summaryContainer}>
+                <Text>You selected</Text>
+                <NumberContainer>
+                    {selectedNumber}
+                </NumberContainer>
+                <Button title="START GAME" />
+            </Card>)
     }
 
     return (
@@ -107,6 +116,10 @@ const styles = StyleSheet.create({
     input: {
         width: 100,
         textAlign: 'center'
+    },
+    summaryContainer: {
+        marginTop: 20,
+        alignItems: 'center'
     }
 })
 
