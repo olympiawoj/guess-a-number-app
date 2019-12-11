064ebd3bc2f3245544c951c45922a31cb5c4406f
olympia <owojcik7@gmail.com>
2019-12-11 16:00:55 -0500

    Using Dimensions in if checks

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from "react"
-import { View, Text, StyleSheet, Button, Alert, ScrollView, FlatList } from "react-native"
+import { View, Text, StyleSheet, Button, Alert, ScrollView, FlatList, Dimensions } from "react-native"
 import NumberContainer from "../components/NumberContainer"
 import Card from "../components/Card"
 import MainButton from "../components/MainButton"
@@ -69,13 +69,18 @@ const GameScreen = props => {
         setPastGuesses(curPastGuesses => [nextNumber.toString(), ...curPastGuesses])
     }
 
+    let listContainerStyle = styles.listContainer
+    if (Dimensions.get('window').width < 350) {
+        listContainerStyle = styles.listContainerBig
+    }
+
     return (
         <View style={styles.screen}>
             <Text style={DefaultStyles.title}>
                 Opponent's Guess
             </Text>
             <NumberContainer>{currentGuess}</NumberContainer>
-            <Card style={styles.buttonContainer}>
+            <Card style={Dimensions.get('window').height > 600 ? styles.buttonContainer : styles.buttonContainer}>
                 <MainButton onPress={nextGuessHandler.bind(this, 'lower')} >
                     <Ionicons name="md-remove" size={24} color="white" />
                 </MainButton>
@@ -85,7 +90,7 @@ const GameScreen = props => {
                 </MainButton>
 
             </Card>
-            <View style={styles.listContainer}>
+            <View style={listContainerStyle}>
                 {/* <ScrollView contentContainerStyle={styles.list}>
                     {pastGuesses.map((guess, index) => renderListItem(guess, pastGuesses.length - index))}
                 </ScrollView> */}
@@ -104,15 +109,20 @@ const styles = StyleSheet.create({
     buttonContainer: {
         flexDirection: 'row',
         justifyContent: 'space-around',
-        marginTop: 20,
+        marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
         width: 400,
         maxWidth: '90%',
         borderColor: 'red',
 
     },
+
+    listContainerBig: {
+        flex: 1,
+        width: '80%'
+    },
     listContainer: {
-        width: '60%',
-        flex: 1
+        flex: 1,
+        width: '60%'
     },
     list: {
         // alignItems: 'center',
