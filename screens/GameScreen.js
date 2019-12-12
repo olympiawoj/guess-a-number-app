d9b4dd4135c9bfb92bee4643f8f33032cee93147
olympia <owojcik7@gmail.com>
2019-12-12 08:37:19 -0500

    Introduce Expo's ScreenOrientation API

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -6,6 +6,7 @@ import MainButton from "../components/MainButton"
 import BodyText from "../components/BodyText"
 import DefaultStyles from "../constants/default-styles"
 import { Ionicons } from '@expo/vector-icons'
+import { ScreenOrientation } from 'expo'
 
 const generateRandomBetween = (min, max, exclude) => {
     min = Math.ceil(min)
@@ -27,6 +28,8 @@ const renderListItem = (listLength, itemData) => (
 )
 
 const GameScreen = props => {
+
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT)
 
     const initialGuess = generateRandomBetween(1, 100, props.userChoice)
     const [currentGuess, setCurrentGuess] = useState(initialGuess)
